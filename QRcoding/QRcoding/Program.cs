0d4e7736f06748dfead775bb5a3f45b6ef3d6fa9
cipriandrei936 <44920094+cipriandrei936@users.noreply.github.com>
2018-11-11 08:45:42 +0200

    Add files via upload

---
+++ QRcoding/QRcoding/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Drawing;
+using MessagingToolkit.QRCode.Codec.Data;
+
+namespace QRcoding
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Bitmap bmp = null;
+            
+            startEncode(5, bmp);
+
+
+        }
+
+        public static void startEncode(int id, Bitmap bmp)
+        {
+            string fileName = "C:\\Users\\chilloutandrew\\Desktop\\exporthtml\\dQRcode_id5.jpeg";
+            MessagingToolkit.QRCode.Codec.QRCodeEncoder encoder = new MessagingToolkit.QRCode.Codec.QRCodeEncoder();
+            encoder.QRCodeScale = 10;
+            bmp = encoder.Encode(id.ToString());
+            bmp.Save(fileName, System.Drawing.Imaging.ImageFormat.Jpeg);
+        }
+ 
+    }
+}
